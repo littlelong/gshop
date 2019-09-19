b8c9dbca6ac3f6b1116b9a61a0ad29718763cd17
zxl <974224908@qq.com>
2019-09-19 13:00:36 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <RouterView />
+    <FooterGuide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+export default {
+  mounted() {
+    this.$store.dispatch('getAddress')
+  },
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.app
+  width 100%
+  height 100%
+  background #f5f5f5
+</style>
