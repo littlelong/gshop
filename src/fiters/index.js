ca50ddbfecd4c0999b62593e664f26f3d869a741
zxl <974224908@qq.com>
2019-09-23 15:09:25 +0800

    优化

--- src/fiters/index.js
+++ src/fiters/index.js
@@ -1,7 +1,7 @@
-//import Vue from 'vue'
+import Vue from 'vue'
 //import moment from 'moment'
-//import format from 'date-fns/format'
-//Vue.filter('date-format',function(value,formatStr='YYYY-MM-DD HH:mm:ss'){
+import format from 'date-fns/format'
+Vue.filter('date-format',function(value,formatStr='yyyy-MM-dd HH:mm:ss'){
       // return moment(value).format(formatStr)
-  //    return format(value, formatStr)
-//})
+      return format(value, formatStr)
+})
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import store from './store'
 import './mock/mockServer'
 import loading from './common/imgs/loading.gif'
-//import './fiters' // 加载过滤器
+import './fiters' // 加载过滤器
 
 Vue.component(Button.name,Button) //<mt-button>
 Vue.use(VueLazyload,{
